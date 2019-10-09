515a24bfb9d708ec58f3ae499be26425420f7c02
TieKie <letrananhkiet@gmail.com>
2019-10-10 04:00:22 +0900

    index, gender is Done! listHobby is doing!

--- index.js
+++ index.js
@@ -6,24 +6,3 @@ $(function() {
     loaderPage();
 });
 
-$(document).ready(function(){
-    var header = $('body');
-    
-    var backgrounds = new Array(
-        'url(image/b.jpg)'
-        , 'url(image/b1.jpg)'
-        , 'url(image/b2.jpg)'
-    );
-    
-    var current = 0;
-    
-    function nextBackground() {
-        current++;
-        current = current % backgrounds.length;
-        header.css('background-image', backgrounds[current]);
-    }
-    setInterval(nextBackground, 4000);
-    
-    header.css('background-image', backgrounds[0]);
-    });
-
